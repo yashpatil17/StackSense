f7605b35b5fe34be9a78c6b2e5478b9682f92b3c
Pratik Desai <pratikdesai399@gmail.com>
2024-04-28 13:09:54 -0500

    UI Updates

--- client/src/App.js
+++ client/src/App.js
@@ -3,30 +3,41 @@ import axios from "axios";
 import Navbar from "./components/NavBar";
 
 function App() {
-  const [inputData, setInputData] = useState("");
+  const [question, setQuestion] = useState("");
+  const [description, setDescription] = useState("");
+  const [previousAction, setPreviousAction] = useState("");
   const [predictions, setPredictions] = useState([]);
   const [similar, setSimilar] = useState([]);
 
-  const handleInputChange = (event) => {
-    setInputData(event.target.value);
+  const handleQuestionChange = (event) => {
+    setQuestion(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handlePreviousActionChange = (event) => {
+    setPreviousAction(event.target.value);
   };
 
   const handleSubmit = async () => {
     try {
       console.log("SUBMIT");
+      const inputDataToSend = `${question} ${description} ${previousAction}`; // Combine all three inputs
       const predictResponse = await axios.post(
         "http://127.0.0.1:5000/predict",
         {
-          input_data: inputData,
+          input_data: inputDataToSend,
         }
       );
       console.log(predictResponse.data.prediction[0]);
       setPredictions(predictResponse.data.prediction[0]);
-
+  
       const similarResponse = await axios.post(
         "http://127.0.0.1:5000/similar_questions",
         {
-          input_data: inputData,
+          input_data: inputDataToSend,
         }
       );
       console.log(similarResponse.data.similar_questions);
@@ -35,12 +46,23 @@ function App() {
       console.error("Error:", error);
     }
   };
+  
 
   return (
     <div>
       <Navbar />
       <h1>Predict Tags</h1>
-      <textarea value={inputData} onChange={handleInputChange} />
+      <label>Question:</label>
+      <br />
+      <input type="text" value={question} onChange={handleQuestionChange} />
+      <br />
+      <label>Description:</label>
+      <br />
+      <input type="text" value={description} onChange={handleDescriptionChange} />
+      <br />
+      <label>Previous Action:</label>
+      <br />
+      <input type="text" value={previousAction} onChange={handlePreviousActionChange} />
       <br />
       <button onClick={handleSubmit}>Predict</button>
       <br />
